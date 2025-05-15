05dcf6b7685884ed5d2e2d0b22705d262540b601
Chan0322 <70271230+Chan0322@users.noreply.github.com>
2025-05-15 23:53:41 +0900

    [Bronze V] Title: 사분면 고르기, Time: 176 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+
+	Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		Main bj = new Main();
+		bj.quadrantTest();
+	}
+	
+	public void quadrantTest() {
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		int quad = 0;
+		
+		if(x>=0&&y>=0) {
+			quad = 1;
+		}else if(x<=0&&y>=0) {
+			quad = 2;
+		}else if(x<=0&&y<=0) {
+			quad = 3;
+		}else if(x>=0&&y<=0){
+			quad = 4;
+		}
+		
+		switch(quad) {
+		case 1:
+			System.out.println("1");
+			break;
+		case 2:
+			System.out.println("2");
+			break;
+		case 3:
+			System.out.println("3");
+			break;
+		case 4:
+			System.out.println("4");
+		}
+	}
+}
