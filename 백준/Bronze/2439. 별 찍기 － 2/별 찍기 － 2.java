481b6c2e5dba586ab84cf1e51907449dfe69f64f
Chan0322 <70271230+Chan0322@users.noreply.github.com>
2025-05-18 01:55:55 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 276 ms, Memory: 20760 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		revStar();
+	}
+	
+	public static void revStar() {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			for(int j=1; j<n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<=i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
