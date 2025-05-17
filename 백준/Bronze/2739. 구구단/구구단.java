5a248851f7280bad4a0f1ebee2d416fd97b525b0
Chan0322 <70271230+Chan0322@users.noreply.github.com>
2025-05-17 22:31:57 +0900

    [Bronze V] Title: 구구단, Time: 192 ms, Memory: 18564 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		mulTest();
+	}
+	
+	public static void mulTest() {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=1; i<10; i++) {
+			System.out.println(n + " * " + i + " = " + n*i);
+		}
+	}
+
+}
