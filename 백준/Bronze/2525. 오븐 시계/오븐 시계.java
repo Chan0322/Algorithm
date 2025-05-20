11a1f661504e14f53a1a460985ef9eaa5f13565e
Chan0322 <70271230+Chan0322@users.noreply.github.com>
2025-05-20 18:25:06 +0900

    [Bronze III] Title: 오븐 시계, Time: 188 ms, Memory: 18492 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,36 +1,12 @@
-import java.util.Scanner;
-
-public class Main {
-
-	public static void main(String[] args) {
-		ovenTest();
-	}
-
-	public static void ovenTest() {
-		Scanner sc = new Scanner(System.in);
-		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		
-		int C = sc.nextInt();
-		
-		int min = 0;
-		int h = 0;
-		int m = 0;
-		
-		if(A<=23&&A>=0 && B>=0&&B<=59) {
-			min = A*60 + B + C; //조리 시간 연산.
-			h = min/60; //시
-			m = min%60; //분
-			if(h>=24) { // 기존 코드에서 h>=23을 수정.
-				h = h-24;
-			}
-		}
-		
-		
-		
-		
-		System.out.println(h + " " + m);
-		
-	}
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
